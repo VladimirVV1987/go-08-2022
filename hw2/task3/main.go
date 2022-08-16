2efbc94c27ea73de98296f388c6b84aaacc3fc73
VladimirVV1987 <vladimirvvrg@yandex.ru>
2022-08-16 23:13:26 +0300

    4,1. Познакомьтесь с алгоритмом сортировки вставками. Напишите приложение, которое принимает на вход набор целых чисел и отдаёт на выходе его же в отсортированном виде.

--- hw2/task3/main.go
+++ hw2/task3/main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	var a, b, c, qwerty int
-		
+
 	fmt.Print("Введите трехзначное число:  ")
 	fmt.Scanln(&qwerty)
 
 	a = qwerty / 100 % 10
 	b = qwerty / 10 % 10
 	c = qwerty / 1 % 10
-	
+
 	fmt.Println("Сотни: ", a)
 	fmt.Println("десятки: ", b)
 	fmt.Println("единицы: ", c)
---
+++ hw4/main.go
@@ -0,0 +1,50 @@
+/*
+Урок 4. Сложные типы данных: массивы, слайсы и мапы
+1. Познакомьтесь с алгоритмом сортировки вставками. Напишите приложение, которое принимает на вход набор целых чисел и
+отдаёт на выходе его же в отсортированном виде.
+Сохраните код, он понадобится нам в дальнейших уроках.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c, d, e, f int
+	fmt.Println("Введите число a: ")
+	fmt.Scanln(&a)
+	fmt.Println("Введите число b: ")
+	fmt.Scanln(&b)
+	fmt.Println("Введите число c: ")
+	fmt.Scanln(&c)
+	fmt.Println("Введите число d: ")
+	fmt.Scanln(&d)
+	fmt.Println("Введите число e: ")
+	fmt.Scanln(&e)
+	fmt.Println("Введите число f: ")
+	fmt.Scanln(&f)
+	fmt.Println("Вы ввели следующие значения: ", a, b, c, d, e, f)
+
+	/*
+	   a := 2
+	   b := 7
+	   c := 5
+	   d := 11
+	   e := -1
+	   f := 0 */
+	arr := []int{a, b, c, d, e, f}
+	InsertionSort(arr)
+	fmt.Println(arr)
+}
+
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		x := arr[i]
+		j := i
+		for ; j >= 1 && arr[j-1] > x; j-- {
+			arr[j] = arr[j-1]
+		}
+		arr[j] = x
+	}
+}
